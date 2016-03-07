bb68fc19d335f264be20dfc921afb11145648921
mihaivisu <mihaivisuian@gmail.com>
2016-03-07 12:02:23 +0000

    fix some indentation and intro changes

--- server/public/javascripts/listen.js
+++ server/public/javascripts/listen.js
@@ -3,27 +3,27 @@
 var app = angular.module('listen', []);
 
 app.controller("ListenController", ['$scope', '$http', function($scope, $http){
-  $scope.img = '/images/talk1.png';
-
-  $scope.changeIn = function(){
-    $scope.img = '/images/talk2.png';
-  }
-
-  $scope.changeOut = function(){
     $scope.img = '/images/talk1.png';
-  }
 
-	$scope.listen = function(){
-      console.log('Listen function');
-      //send post request to get the file and its content
-      $http.post('http://8e5b26b4.ngrok.io/listen', {})
-        .success(function(data, status){
-          console.log(data);
-          console.log('Listen send OK');
-        })
-        .error(function(data, status){
-          console.log("Error to the post request for the file content in angular");
-        });
-	}
-}]);
+        $scope.changeIn = function(){
+            $scope.img = '/images/talk2.png';
+        };
+
+        $scope.changeOut = function(){
+            $scope.img = '/images/talk1.png';
+        };
+
+    	$scope.listen = function(){
+            console.log('Listen function');
+            //send post request to get the file and its content
+            $http.post('http://8e5b26b4.ngrok.io/listen', {})
+                .success(function(data, status){
+                    console.log(data);
+                    console.log('Listen send OK');
+                })
+                .error(function(data, status){
+                    console.log("Error to the post request for the file content in angular");
+                });
+    	};
+    }]);
 })();
